5b7f2c3caa059f785d75d2edd9c043b09a62333a
Raul Alvarado <raulalvarado.23@gmail.com>
2021-12-28 20:27:12 +0100

    se añaden los ejercicios 9 y 10

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
+const palindromes = function (inputWord) {
+    inputWord = inputWord.toLowerCase().replace(/[^A-Za-z]/g, "");
+    let reversedWord = inputWord.split("").reverse().join("");
 
+    return inputWord == reversedWord;
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(limit) {
 
+    if(limit<0) return "OOPS"
+
+    fibonacciSerie = [1, 1];
+
+    for(let i=1; i<limit; i++){
+        fibonacciSerie.push(fibonacciSerie[i]+fibonacciSerie[i-1]);
+    }
+
+    return fibonacciSerie[limit-1];
 };
 
 // Do not edit below this line

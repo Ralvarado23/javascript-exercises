0134e2d0489ff1d3d6165da30b547ea073911f55
Raul Alvarado <raulalvarado.23@gmail.com>
2021-12-28 12:31:58 +0100

    se ha añadido la solución para el ejercicio 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,54 @@
 const add = function() {
-	
+  total = 0;
+  
+  for (let i=0; i < arguments.length; i++) {
+    total += arguments[i];
+  }
+
+  return total;
 };
 
 const subtract = function() {
-	
+  return arguments[0]-arguments[1];
 };
 
-const sum = function() {
+const sum = function(inputArray) {
+  total = 0;
 	
+  for (let i=0; i < inputArray.length; i++) {
+    total += inputArray[i];
+  }
+  
+  return total;
 };
 
-const multiply = function() {
+const multiply = function(inputArray) {
 
+if(inputArray.length == 0){
+  return 0;
+}
+
+total = 1;
+
+for (let i=0; i < inputArray.length; i++) {
+  total *= inputArray[i];
+}
+
+return total;
 };
 
 const power = function() {
-	
+	return arguments[0]**arguments[1];
 };
 
-const factorial = function() {
-	
+const factorial = function(inputNumber) {
+  total = 1;
+
+	for(let i = 1; i<=inputNumber; i++){
+    total *= i;
+  }
+
+  return total;
 };
 
 // Do not edit below this line

fff8f3439bcf7c01363f916153c791fd3e88d27a
Raul Alvarado <raulalvarado.23@gmail.com>
2021-12-31 12:33:39 +0100

    se ha añadido la solución para el snakeCase

--- snakeCase/snakeCase.js
+++ snakeCase/snakeCase.js
@@ -1,5 +1,14 @@
-const snakeCase = function() {
+const snakeCase = function(inputString) {
 
+    //Se eliminan los caracteres especiales y los espacios al principio y al final
+    inputString = inputString.replace(/[^\w\s]|_/g, " ").trim();
+
+    //Correción para los camel case
+    if(inputString.indexOf(" ")<0){
+        inputString = inputString.replace(/([A-Z])/g, " $1");
+    }
+    
+    return inputString.toLowerCase().replace(/\s+/g, "_");
 };
 
 // Do not edit below this line
